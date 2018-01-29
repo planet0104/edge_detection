4af74135aa40633c743f8121fd88461e76e79841
Jia Ye <planet0104@gmail.com>
2018-01-29 13:10:26 +0800

    修复双线问题
    修复双线问题

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ extern crate lodepng;
 use lodepng::RGB;
 
 fn main() {
-    let bitmap = lodepng::decode24_file("0.png").unwrap();
+    let bitmap = lodepng::decode24_file("rust.png").unwrap();
     let (width, height) = (bitmap.width, bitmap.height);
     println!("width={},height={}", width, height);
 
-    let buffer = edge_detection(&bitmap.buffer, width, height, 127.0);
+    let buffer = edge_detection(&bitmap.buffer, width, height, 127.5);
     
-    lodepng::encode24_file("0_out.png", &buffer, width, height).unwrap();
+    lodepng::encode24_file("rust_out.png", &buffer, width, height).unwrap();
 }
 
 //边缘检测
@@ -19,54 +19,37 @@ fn main() {
 //返回: 黑底百色边缘的图像数据
 fn edge_detection(buffer:&Vec<RGB<u8>>, width:usize, height:usize, threshold:f32)->Vec<RGB<u8>>{
     let mut out_buffer:Vec<RGB<u8>> = vec![];
-
-    let mut row_first_binary = true; //当前行第一个是否是双极细胞
     let mut i = 0;
 
     for _row in 0..height{
         //第一列是否是双极细胞
-        let mut col_binary = row_first_binary;
         for _col in 0..width{
-            //右下4个像素
-            match((calc_pixel(buffer.get(i+1), !col_binary, &buffer[i], threshold)
-                        + calc_pixel(buffer.get(i+2), col_binary, &buffer[i], threshold)
-                        + calc_pixel(buffer.get(i+width), !col_binary, &buffer[i], threshold)
-                        + calc_pixel(buffer.get(i+width+width), col_binary, &buffer[i], threshold)
+            //4个像素
+            //双极细胞 给光ON，撤光OFF => 超过阈值:255
+            //水平细胞 亮光抑制，弱光增强，和双极细胞正好相反 => 超过阈值:-255
+            match((calc_pixel(buffer.get(i).unwrap_or(&buffer[i]), 255.0, threshold)
+                        + calc_pixel(buffer.get(i+1).unwrap_or(&buffer[i]), -255.0, threshold)
+                        +calc_pixel(buffer.get(i).unwrap_or(&buffer[i]), 255.0, threshold)
+                        + calc_pixel(buffer.get(i+width).unwrap_or(&buffer[i]), -255.0, threshold)
                         )/4.0) as i32{ //(Sum(..pixel..)/255*4)*255 = Sum(..pixel..)/4
                 0 => out_buffer.push(RGB::new(0, 0, 0)),
                 _ => out_buffer.push(RGB::new(255, 255, 255)),
             }
-
-            //下一列是否是双极细胞
-            col_binary = !col_binary;
             i += 1;
         }
-        //下一行第一个是否是双极细胞
-        row_first_binary = !row_first_binary;
     }
 
     out_buffer
 }
 
 //计算每个像素的输出
-//双极细胞(binary) 给光ON，撤光OFF
-//水平细胞(!binary) 亮光抑制，弱光增强，和双极细胞正好相反。
 // p: 像素
-// binary: true说明是像素代表双极细胞, false说明像素代表水平细胞
-// c: 如果像素没有找到，用此像素替换。
+// out: 超过阈值细胞的输出
 // threshold: 阈值0~255
-fn calc_pixel(p:Option<&RGB<u8>>, binray:bool, c:&RGB<u8>, threshold:f32)->f32{
-    let pixel = match p{
-        Some(pixel) => pixel,
-        None => c
-    };
+fn calc_pixel(pixel:&RGB<u8>, out:f32, threshold:f32)->f32{
     //二值化以后根据双极细胞、水平细胞返回输出值
     if 0.299*pixel.r as f32+0.587*pixel.g as f32+0.114*pixel.b as f32>threshold{
-        if binray{
-            255.0 //强光双击细胞返回ON
-        }else{
-            -255.0 //强光水平细胞返回OFF
-        }
+        out
     }else{
         0.0 //弱光都不返回
     }
---
+++ src/test.rs
@@ -0,0 +1,110 @@
+extern crate lodepng;
+use lodepng::RGB;
+
+fn main() {
+    let bitmap = lodepng::decode24_file("rust.png").unwrap();
+    let (width, height) = (bitmap.width, bitmap.height);
+    println!("width={},height={}", width, height);
+
+    let mut buffer:Vec<RGB<u8>> = vec![RGB::new(0, 0, 0); width*height];
+
+    edge_detection_h(&bitmap.buffer, &mut buffer, width, height, 127.0);
+    edge_detection_v(&bitmap.buffer, &mut buffer, width, height, 127.0);
+    
+    lodepng::encode24_file("test_out.png", &buffer, width, height).unwrap();
+}
+
+//边缘检测
+//buffer: 图像数据
+//height: 图像高度
+//width: 图像宽度
+//threshold: 阈值0~255
+//返回: 黑底百色边缘的图像数据
+fn edge_detection_h(buffer:&Vec<RGB<u8>>, out_buffer:&mut Vec<RGB<u8>>, width:usize, height:usize, threshold:f32){
+    let mut row_first_binary = true; //当前行第一个是否是双极细胞
+    let mut i = 0;
+
+    for _row in 0..height{
+        //第一列是否是双极细胞
+        let mut col_binary = row_first_binary;
+        for _col in 0..width{
+
+            match((calc_pixel(buffer.get(i+width), !col_binary, &buffer[i], threshold)
+                        + calc_pixel(buffer.get(i+width+1), col_binary, &buffer[i], threshold)
+                        )/2.0) as i32{ //(Sum(..pixel..)/255*4)*255 = Sum(..pixel..)/4
+                0 => {
+                    //out_buffer[i].r = 0;
+                    //out_buffer[i].g = 0;
+                    //out_buffer[i].b = 0;
+                }
+                _ => {
+                    out_buffer[i].r = 255;
+                    out_buffer[i].g = 255;
+                    out_buffer[i].b = 255;
+                }
+            }
+            //下一列是否是双极细胞
+            col_binary = !col_binary;
+            i += 1;
+        }
+        //下一行第一个是否是双极细胞
+        row_first_binary = !row_first_binary;
+    }
+}
+
+fn edge_detection_v(buffer:&Vec<RGB<u8>>, out_buffer:&mut Vec<RGB<u8>>, width:usize, height:usize, threshold:f32){
+    let mut row_first_binary = true; //当前行第一个是否是双极细胞
+    let mut i = 0;
+
+    for _row in 0..height{
+        //第一列是否是双极细胞
+        let mut col_binary = row_first_binary;
+        for _col in 0..width{
+
+            match((
+                        calc_pixel(buffer.get(i+width), !col_binary, &buffer[i], threshold)
+                        + calc_pixel(buffer.get(i+width+width), col_binary, &buffer[i], threshold)
+                        )/2.0) as i32{ //(Sum(..pixel..)/255*4)*255 = Sum(..pixel..)/4
+                0 => {
+                    //out_buffer[i].r = 0;
+                    //out_buffer[i].g = 0;
+                    //out_buffer[i].b = 0;
+                }
+                _ => {
+                    out_buffer[i].r = 255;
+                    out_buffer[i].g = 0;
+                    out_buffer[i].b = 0;
+                }
+            }
+            //下一列是否是双极细胞
+            col_binary = !col_binary;
+            i += 1;
+        }
+        //下一行第一个是否是双极细胞
+        row_first_binary = !row_first_binary;
+    }
+}
+
+//计算每个像素的输出
+//双极细胞(binary) 给光ON，撤光OFF
+//水平细胞(!binary) 亮光抑制，弱光增强，和双极细胞正好相反。
+// p: 像素
+// binary: true说明是像素代表双极细胞, false说明像素代表水平细胞
+// c: 如果像素没有找到，用此像素替换。
+// threshold: 阈值0~255
+fn calc_pixel(p:Option<&RGB<u8>>, binray:bool, c:&RGB<u8>, threshold:f32)->f32{
+    let pixel = match p{
+        Some(pixel) => pixel,
+        None => c
+    };
+    //二值化以后根据双极细胞、水平细胞返回输出值
+    if 0.299*pixel.r as f32+0.587*pixel.g as f32+0.114*pixel.b as f32>threshold{
+        if binray{
+            255.0 //强光双击细胞返回ON
+        }else{
+            -255.0 //强光水平细胞返回OFF
+        }
+    }else{
+        0.0 //弱光都不返回
+    }
+}
